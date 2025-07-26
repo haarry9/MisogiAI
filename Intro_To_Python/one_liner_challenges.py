b1e7038c026b549f17b287d8b294f8e48c55140e
haarry9 <hspv99@gmail.com>
2025-07-26 10:48:57 +0000

    One Liner Challenges

---
+++ Intro_To_Python/one_liner_challenges.py
@@ -0,0 +1,44 @@
+# 1. Filter a list to find words of a specific length
+
+words = ["apple", "banana", "kiwi", "strawberry", "orange", "grape"]
+
+long_words = [word for word in words if len(word) > 5]
+print(long_words)
+
+# 2. Convert a list of strings to a list of their lengths
+
+words = ["hello", "world", "from", "python"]
+
+words_lengths = list(map(len, words))
+print(words_lengths)
+
+# 3. Find all numbers from a list that are divisible by 3
+
+from functions import reduce
+
+numbers = [1, 5, 9, 12, 17, 21, 25]
+
+product = reduce(lambda x,y: x*y, numbers)
+
+print(product)
+
+# 4. Find all numbers from a list that are divisible by 3
+
+numbers = [1, 5, 9, 12, 17, 21, 25]
+
+divisible_by_three = list(filter(lambda x: x % 3 == 0, numbers))
+
+print(divisible_by_three)
+
+# 5. Extract names of people older than 25 from a list of dictionaries
+
+people = [
+    {"name": "Alice", "age": 30},
+    {"name": "Bob", "age": 22},
+    {"name": "Charlie", "age": 28},
+    {"name": "Diana", "age": 24}
+]
+
+adult_names = [person["name"] for person in people if person["age"] > 25]
+
+print(adult_names)
